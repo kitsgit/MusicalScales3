a5f28d2c331c6e0b8244bc4a3565975115ea31f0
kitsgit <christina.aswez@gmail.com>
2020-02-25 16:45:40 +0530

    Add project files.

---
+++ MusicalScales3/Collection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicalScales3
+{
+    abstract class AbsCollection
+    {
+        public string key;
+        public string name;
+    }
+
+    class Collection : AbsCollection
+    {
+
+    }
+
+    class Scale : AbsCollection
+    {
+        public string root;
+
+    }
+
+    class Note : Scale
+    {
+        public string nname;
+
+        public void print()
+        {
+
+        }
+    }
+}
